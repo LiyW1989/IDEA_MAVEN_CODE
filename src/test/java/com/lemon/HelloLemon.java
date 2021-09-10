cbbd1f095c5046d14797802b4526e586ab3200cc
liyW1989 <wanglong-1989@163.com>
2021-09-10 23:20:22 +0800

    first commit

---
+++ src/test/java/com/lemon/HelloLemon.java
@@ -0,0 +1,15 @@
+package com.lemon;
+
+/**
+ * @Project: idea_git_test
+ * @Site: http://www.lemonban.com
+ * @Author: l
+ * @Create: 2021-09-10 23:14
+ * @Desc：
+ **/
+
+public class HelloLemon {
+    public static void main(String[] args) {
+        System.out.println("Hello Lemon");
+    }
+}
